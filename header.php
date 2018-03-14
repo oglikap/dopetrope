15aed7adc17c6bfb344dd8a510f6db767ef8dd11
oglikap <kroon.rj@gmail.com>
2018-03-15 00:48:15 +0100

    integrated the navbar in wp

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<!--
+	Dopetrope by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<!--<link rel="stylesheet" href="assets/css/main.css" />-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+    <?php wp_head(); ?>
+	</head>
+  <body class="homepage">
+    <div id="page-wrapper">
